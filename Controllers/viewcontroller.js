aeee55b787d6aba7f2823cdb842faa18f6b5efa6
ahadg <48055296+ahadg@users.noreply.github.com>
2021-05-02 12:17:34 +0500

    Update viewcontroller.js

--- Controllers/viewcontroller.js
+++ Controllers/viewcontroller.js
@@ -2,11 +2,7 @@ const tourmodel = require('../Models/tourmodel');
 
 
 exports.overview = async (req,res) => {
-   // console.log(req.body);
      const tours = await tourmodel.find(req.body);
-      //  difficulty : 'easy);
-   //  console.log(tours);
-    // console.log(tour.name);
     res.status(201).render('overview.ejs', {
         tour : tours
     });
